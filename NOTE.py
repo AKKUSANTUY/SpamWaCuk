711ef6c470a4373fae1b430a927cceb87cc34ece
AKKUSANTUY <79558956+AKKUSANTUY@users.noreply.github.com>
2021-03-06 11:43:19 +0700

    Add files via upload

---
+++ NOTE.py
@@ -0,0 +1,62 @@
+#_*_coding:UTF-8_*_
+
+import random
+import sys
+import time
+def type(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+type('ASSALAMUALAIKUM ...  SUDAH TIDAK BISA SEMUA? KONTAK AUTHOR, || SKILLMU BERGUNA UNTUK BACOTMU \n TAPI BACOTMU TIDAK BERGUNA UNTUK SKILLMU \n MERENDAH SAMPAI TIDAK ADA YANG MERENDAHAKAN \n
+1. "Jangan pernah meremehkan tekad seorang anak yang kaya waktu dan miskin uang".
+\n
+2. "Ada beberapa sumber energi yang begitu kuat sebagai mahasiswa yang suka menunda-nunda".
+\n
+3. "Kebanyakan peretas masih muda, karena orang cenderung mudah beradaptasi. Selama Anda tetap bisa beradaptasi, Anda selalu bisa menjadi peretas yang baik".
+\n
+4. "Bukan monster yang harus kita takuti; itu adalah orang-orang yang tidak mengenali monster yang sama di dalam diri mereka".
+\n
+5. "Faktanya ya, mereka sangat mudah. Masalahnya adalah Anda tidak harus melindungi diri dari orang bodoh. Anda harus melindungi diri terhadap orang seperti saya".
+\n
+6. "Apa yang dilakukan peretas adalah mencari tahu teknologi dan bereksperimen dengannya dengan cara yang tidak pernah dibayangkan banyak orang. Mereka juga memiliki keinginan yang kuat untuk membagikan informasi ini dengan orang lain dan menjelaskannya kepada orang-orang yang satu-satunya kualifikasi mungkin adalah keinginan untuk belajar".
+\n
+7. "Kebodohan manusia, itulah sebabnya peretas selalu menang". 
+\n
+8. "Ingat, ketika Anda terhubung dengan komputer lain, Anda terhubung ke setiap komputer yang terhubung dengan komputer.”
+\n
+9. "Peretas adalah bakat. Anda tidak akan mempelajarinya di sekolah. Ini seperti menjadi Messi atau C. Ronaldo. Jika Anda dilahirkan untuk menjadi seorang peretas, itu adalah takdir Anda. Jika tidak, Anda akan diretas".
+\n
+10. "Peretasan komputer seperti ikatan kimia yang mengikat kita semua".
+\n
+11. "Anda tidak dapat meretas takdir, kekuatan kasar… Anda perlu pintu belakang, saluran samping ke dalam hidup".
+\n
+12. "Jika Anda adalah peretas yang baik, semua orang mengenal Anda. Tetapi, jika Anda peretas yang hebat tidak ada yang mengenal Anda".
+\n
+13. "Lawan dulu dan sekarang memiliki kelemahan esensial yang sama tentang mereka. Pertama mereka ingin menggunakan Anda, kemudian ingin menjadi Anda, kemudian mereka ingin menghabisi Anda".
+\n
+14. "Sains adalah apa yang kami pahami dengan cukup baik untuk dijelaskan ke komputer; seni adalah segalanya".
+\n
+15. “Tidak ada yang tidak baik atau buruk tentang pengetahuan itu sendiri; moralitas terletak pada penerapan pengetahuan.”
+\n
+16. “Bagi saya, komputer itu alat yang paling luar biasa yang pernah kita miliki. Ini setara dengan sepeda untuk pikiran kita.”
+\n
+17. “Saya percaya pada dunia di mana semua hal ini bisa terjadi, bahkan jika saya harus melakukannya sendiri.”
+\n
+18. “Hal terpenting tentang teknologi adalah bagaimana hal itu mengubah orang.”
+\n
+19. “Pelajari hari Ini, terapkan hari Ini.”
+\n
+20. “Anda hanya berjarak satu klik untuk menemukan diri sejati anda.”
+\n
+21. “Saat berjudi, seseorang harus tahu lawan mereka, dan kelemahan mereka. Terutama, jika lawan itu adalah mesin.”
+\n
+ 22. "Percayalah pada getaran yang Anda dapatkan, energi tidak berbohong".
+\n
+23. "Ketika kita semua memiliki akses ke informasi, kita kuat. Ketika kita kuat, kita tetap memiliki kekuatan untuk melakukan hal yang mustahil".
+\n
+24. "Kami melakukan hal-hal yang kami lakukan untuk rakyat, bukan untuk kepentingan kita sendiri seperti sebuah ketenaran".
+\n
+25. "Pemerintah dunia tidak bisa terus menerus menekan Anonymous untuk selamanya, karena jumlah mereka terus berkembang setiap hari".
+\n
+[!] FROM ZERO TO HERO[!] \n SALAM DARI  \n °SANTRI CYBER SANTUY')
